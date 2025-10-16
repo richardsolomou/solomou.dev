9408df50d6333c623be0e8520a78050d0398f97f
Richard Solomou <richard@solomou.io>
2025-10-16 23:29:49 +0300

    fix canonical url

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,45 +6,55 @@ import { NotFound } from "~/components/not-found";
 import { seo } from "~/lib/seo";
 import appCss from "~/styles/app.css?url";
 
+const CANONICAL_DOMAIN = "https://solomou.dev";
+
+const getCanonicalUrl = (pathname: string) => `${CANONICAL_DOMAIN}${pathname}`;
+
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      ...seo({
-        title: "solomou.dev",
-        description:
-          "Full-Stack Engineer 👨‍💻, proud dad 👶, and coffee junkie ☕️",
-      }),
-    ],
-    links: [
-      { rel: "stylesheet", href: appCss },
-      {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/apple-touch-icon.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/favicon-32x32.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        href: "/favicon-16x16.png",
-      },
-      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
-      { rel: "icon", href: "/favicon.ico" },
-    ],
-  }),
+  head: ({ match }) => {
+    const canonicalUrl = getCanonicalUrl(match.pathname);
+
+    return {
+      meta: [
+        {
+          charSet: "utf-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        ...seo({
+          title: "solomou.dev",
+          description:
+            "Full-Stack Engineer 👨‍💻, proud dad 👶, and coffee junkie ☕️",
+          canonical: canonicalUrl,
+        }),
+      ],
+      links: [
+        { rel: "stylesheet", href: appCss },
+        { rel: "canonical", href: canonicalUrl },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+        { rel: "icon", href: "/favicon.ico" },
+      ],
+    };
+  },
   errorComponent: DefaultCatchBoundary,
   notFoundComponent: () => <NotFound />,
   shellComponent: RootDocument,
