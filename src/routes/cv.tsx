10f8fa68ae2c1c9786d6f5f8f13929b8b97925a4
Richard Solomou <richard.s@posthog.com>
2026-01-14 23:38:14 +0100

    update profile picture

--- src/routes/cv.tsx
+++ src/routes/cv.tsx
@@ -56,7 +56,7 @@ function CV() {
             alt="Richard Solomou"
             className="shrink-0 rounded-full print:size-25"
             height={128}
-            src="/images/richard-face.png"
+            src="/images/richard.png"
             width={128}
           />
 
